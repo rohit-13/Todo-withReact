2ac5e9537f5dfc2a43ec9db501f7d3f4fca8f232
Rohit Kumar <rohit.kumar@yellow.ai>
2023-10-04 21:59:19 +0530

    design fix

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,14 +1,23 @@
-export default function Tabs({todoTab, setTodoTab}) {
+export default function Tabs({ todoTab, setTodoTab }) {
   function handleClick(event) {
-    if(event.target.tagName === 'LI') {
+    if (event.target.tagName === "LI") {
       setTodoTab(event.target.textContent.toLowerCase());
     }
   }
   return (
     <ul className="todo-tabs" onClick={handleClick}>
-      <li key={"all"} className={`todo-tab ${todoTab === "all" ? "todo-tab-active" : ""}`}>All</li>
-      <li key={"active"} className={`todo-tab ${todoTab === "active" ? "todo-tab-active" : ""}`}>Active</li>
-      <li key={"completed"} className={`todo-tab ${todoTab === "completed" ? "todo-tab-active" : ""}`}>Completed</li> 
+      <li key={"all"} className="todo-tab">
+        All
+        <div className={`${todoTab === "all" ? "todo-tab-active" : ""}`}></div>
+      </li>
+      <li key={"active"} className="todo-tab">
+        Active
+        <div className={`${todoTab === "active" ? "todo-tab-active" : ""}`}></div>
+      </li>
+      <li key={"completed"} className="todo-tab">
+        Completed
+        <div className={`${todoTab === "completed" ? "todo-tab-active" : ""}`}></div>
+      </li>
     </ul>
   );
-};
+}
